9f1871e804a375226ffc61fc7ef7ff4fbf86cb13
the-johny <jkyalo@gmail.com>
2025-07-05 08:07:43 +0300

    initial commit

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,38 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(private configService: ConfigService) {
+    const databaseUrl = configService.get<string>('DATABASE_URL');
+    
+    if (!databaseUrl) {
+      throw new Error('DATABASE_URL is not defined in environment variables');
+    }
+    
+    super({
+      datasources: {
+        db: {
+          url: databaseUrl,
+        },
+      },
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    (this as any).$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
