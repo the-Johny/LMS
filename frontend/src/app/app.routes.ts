dbdb1c651d5662427c65eb00e99d4f75220bbad3
Yobra28 <briankuruui3768@gmail.com>
2025-07-06 09:05:13 +0300

    git stashing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
-    { path: '', component: LandingComponent }
+    { path: '', component: LandingComponent },
 ];
